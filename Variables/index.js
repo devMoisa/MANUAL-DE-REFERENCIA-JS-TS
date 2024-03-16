c9e7580259213d6b022220c73df14f10ae354b49
devMoisa <moisesboas@gmail.com>
2024-03-16 20:20:27 -0300

    feat: variables

--- Variables/index.js
+++ Variables/index.js
@@ -1,3 +1,15 @@
-var value1 = "";
-let value2 = "";
-const value3 = "";
+// Just declaring/initializing the variables.
+var value1;
+let value2;
+
+// Setting a value into initialized/declared variables.
+value1 = "Value 1";
+value2 = "Value 2";
+
+// Constants can't be initialized and receive a new value after.
+const value3 = "Value 3";
+
+// Loggins
+console.log("var: ", value1);
+console.log("let: ", value2);
+console.log("const: ", value3);
